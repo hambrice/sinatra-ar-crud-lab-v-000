c946d4570b19ecf2eafc007c574be3d7b7618a5a
Brad Hambrice <hambrice@uab.edu>
2018-01-29 03:27:36 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < Sinatra::Base
     erb :new
   end
 
-  post '/posts' do
+  post '/posts/new' do
     Post.create(name: params["name"], content: params["content"])
     #binding.pry
     erb :index
@@ -27,7 +27,6 @@ class ApplicationController < Sinatra::Base
 
   get '/posts/:id' do
     @post = Post.find_by(id: params[:id])
-    #binding.pry
     erb :show
   end
 
