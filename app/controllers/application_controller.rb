6774ae4a1650a28c90bbb46a48f6cb5ad447745b
Brad Hambrice <hambrice@uab.edu>
2018-01-29 04:42:18 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,7 +45,8 @@ class ApplicationController < Sinatra::Base
 
   delete '/posts/:id/delete' do
     @post = Post.find_by(id: params[:id])
-    #binding.pry
+    @post_name = @post.name
+    binding.pry
     @post.destroy
     erb :delete
   end
